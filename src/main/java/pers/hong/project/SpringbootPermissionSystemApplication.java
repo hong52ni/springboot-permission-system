3c60a51adbd13a84817e759462a6b18eb418f9ae
zwh <805258462@qq.com>
2018-11-12 18:51:46 +0800

    m

---
+++ src/main/java/pers/hong/project/SpringbootPermissionSystemApplication.java
@@ -0,0 +1,12 @@
+package pers.hong.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPermissionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPermissionSystemApplication.class, args);
+    }
+}
